0faa8d405271ba76db57706f6cd8eae8aa7add25
Vitor Bonet <vitorbonet@hotmail.com>
2019-09-11 18:46:06 -0300

    primeiro parte

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const readline = require('readline-sync')
+
 function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnSPrefix()
 
     function askAndReturnSearchTerm() {
-        return 'TERM EXEMPLE'
+        return readline.question('Termo para pesquisar na Wikipedia: ')
+    }
+
+    function askAndReturnSPrefix() {
+        const prefixes = ['Quem eh', 'O que eh', 'A historia de']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opção: ')
+        const selectPrefixTerm = prefixes[selectedPrefixIndex]
+
+        return selectPrefixTerm
     }
 
     console.log(content)
