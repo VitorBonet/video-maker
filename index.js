922d63b86acf3e922a1e624b2697668ca963d25a
Vitor Bonet <vitorbonet@hotmail.com>
2019-09-11 20:24:09 -0300

    implementação completa robo texto

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const readline = require('readline-sync')
+const robots = {
+    text: require('./robots/text')
+}
 
-function start() {
+async function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
     content.prefix = askAndReturnSPrefix()
+    await robots.text(content)
 
     function askAndReturnSearchTerm() {
         return readline.question('Termo para pesquisar na Wikipedia: ')
