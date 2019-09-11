52c3aaa92577b8e6ed4cb3aa5d37c94eb26370cd
Vitor Bonet <vitorbonet@hotmail.com>
2019-09-11 18:31:22 -0300

    readline install

---
+++ index.js
@@ -0,0 +1,13 @@
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+
+    function askAndReturnSearchTerm() {
+        return 'TERM EXEMPLE'
+    }
+
+    console.log(content)
+}
+
+start()
