4f61c9183b682a49135d80cf5364dbd89592cfb6
Vitor Bonet <vitorbonet@hotmail.com>
2019-10-05 18:48:10 -0300

    video-robot

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text'),
     state: require('./robots/state'),
-    image: require('./robots/image')
+    image: require('./robots/image'),
+    video: require('./robots/video'),
+    youtube: require('./robots/youtube')
 }
 
 async function start() {
     robots.input()
     await robots.text()
     await robots.image()
+    await robots.video()
+    //await robots.youtube()
 
-    const content = robots.state.load()
-    console.dir(content, {depth: null})
+    //const content = robots.state.load()
+    //console.dir(content, {depth: null})
 
 }
 
